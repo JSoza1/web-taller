234ebbec48d456b6010a647680df2262d1c8a47b
Jsoza1 <jsoza993@gmail.com>
2026-01-08 19:55:10 -0300

    Mejora logo whatsapp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="container footer__container">
-                <p>Desarrollado por <a href="https://sozasystems.vercel.app/" className="neon-link" target="_blank" rel="noopener noreferrer">Soza Systems</a></p>
+                <p>Desarrollado por <a href="https://sozasystems.vercel.app/" className="footer__author" target="_blank" rel="noopener noreferrer">Soza Systems</a></p>
                 <p>Taller Gustavo © 2025</p>
             </div>
         </footer>
